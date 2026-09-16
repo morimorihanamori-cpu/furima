ccd194377ce0427797a937c086c85234051b94e2
morimorihanamori-cpu <morimorihanamori@gmail.com>
2026-09-16 13:43:01 +0900

    Update generate.js

--- functions/api/generate.js
+++ functions/api/generate.js
@@ -51,7 +51,7 @@ export default {
 }
 `;
 
-      const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${env.GEMINI_API_KEY}`, {
+      const response = await fetch('/api/generate', {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
